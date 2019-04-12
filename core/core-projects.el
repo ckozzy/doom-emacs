56809747749060f428a2d0aff046735b6c724854
Ozzy Mint <kelly@shadetree.biz>
2019-04-12 10:24:32 -0600

    removed crlf

--- core/core-projects.el
+++ core/core-projects.el
@@ -8,7 +8,7 @@
         projectile-enable-caching (not noninteractive)
         projectile-known-projects-file (concat doom-cache-dir "projectile.projects")
         projectile-require-project-root t
-        projectile-globally-ignored-files '(".DS_Store" "Icon" "TAGS")
+        projectile-globally-ignored-files '(".DS_Store" "Icon" "TAGS")
         projectile-globally-ignored-file-suffixes '(".elc" ".pyc" ".o")
         projectile-ignored-projects '("~/" "/tmp")
         projectile-kill-buffers-filter 'kill-only-files)
